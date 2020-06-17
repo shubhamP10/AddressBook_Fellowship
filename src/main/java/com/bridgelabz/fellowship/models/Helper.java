d690db17621a8f7210b43527dbcc9bf7b1fe6bb4
“Shubham” <shubhampattar2@gmail.com>
2020-06-17 19:20:42 +0530

    UC3-ADDED Edit user functionality

--- src/main/java/com/bridgelabz/fellowship/models/Helper.java
+++ src/main/java/com/bridgelabz/fellowship/models/Helper.java
@@ -6,7 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Helper {
-    List<Person> p = new ArrayList<Person>();
+    // GLOBAL LIST TO STORE PERSON RECORD
+    List<Person> PERSON = new ArrayList<Person>();
     //	ADD METHOD
     public void addRecord()
     {
@@ -27,16 +28,73 @@ public class Helper {
         System.out.print("Enter state : ");
         state = InputUtil.getStringValue();
 
-        p.add(new Person(fname,lname,address,city,state,phone,zip));
+        PERSON.add(new Person(fname,lname,address,city,state,phone,zip));
     } // END of addRecord()
 
     //	DISPLAY METHOD
     public void displayRecord()
     {
-        for(Person p1: p)
+        for(Person person: PERSON)
         {
-            System.out.println(p1);
+            System.out.println(person);
         }
 
     } // END OF displayRecord
+
+    //	EDIT METHOD
+    public void editRecord()
+    {
+        int id,choice = 0, i=0;
+        String fname,lname,address,city,state,phone,zip;
+        for(Person person: PERSON)
+        {
+            System.out.println("ID: #"+PERSON.indexOf(person)+" : "+person);
+        }
+        System.out.print("\nEnter #ID to Edit Contact : ");
+        id = InputUtil.getIntValue();
+        System.out.println(PERSON.get(id));
+        while(i==0) {
+            System.out.println("What You Want to edit...\n"
+                    + "\t1: Address\n"
+                    + "\t2: city\n"
+                    + "\t3: State\n"
+                    + "\t4: Phone\n"
+                    + "\t5: Zip Code\n"
+                    + "\t6. Save And Exit\n");
+            choice = InputUtil.getIntValue();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter new Street : ");
+                    address = InputUtil.getStringValue();
+                    PERSON.get(id).setAddress(address);
+                    break;
+                case 2:
+                    System.out.print("Enter new City : ");
+                    city = InputUtil.getStringValue();
+                    PERSON.get(id).setCity(city);
+                    break;
+                case 3:
+                    System.out.print("Enter new State : ");
+                    state = InputUtil.getStringValue();
+                    PERSON.get(id).setState(state);
+                    break;
+                case 4:
+                    System.out.print("Enter new Phone : ");
+                    phone = InputUtil.getStringValue();
+                    PERSON.get(id).setPhone(phone);
+                    break;
+                case 5:
+                    System.out.print("Enter new Zip Code : ");
+                    zip = InputUtil.getStringValue();
+                    PERSON.get(id).setZip(zip);
+                    break;
+                case 6:
+                    i=1;
+                    break;
+                default:
+                    System.out.println("Please Enter Valid Option");
+            }
+            System.out.println(PERSON.get(id));
+        }
+    } //end of edit() method
 }
