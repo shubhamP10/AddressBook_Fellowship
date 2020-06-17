40aff6e2aa3dd3696200b5fb428029800cd82c6b
“Shubham” <shubhampattar2@gmail.com>
2020-06-18 00:38:55 +0530

    UC9-PROGRESS for View Person by City and State

---
+++ src/main/java/com/bridgelabz/fellowship/services/Search.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.fellowship.services;
+
+import com.bridgelabz.fellowship.models.Person;
+
+import java.util.Dictionary;
+
+public class Search {
+    public static void searchByCityAndState(Dictionary<String,String> cityDict,Dictionary<String,String> stateDict)
+    {
+        System.out.println("In Progress...");
+    }
+}
