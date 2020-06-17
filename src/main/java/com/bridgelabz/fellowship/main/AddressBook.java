3b6bca97ea74cc9549c4d26cc60a5f5811a255fe
“Shubham” <shubhampattar2@gmail.com>
2020-06-17 17:46:02 +0530

    UC1-ADDED Read Person's Details Functionality

--- src/main/java/com/bridgelabz/fellowship/main/AddressBook.java
+++ src/main/java/com/bridgelabz/fellowship/main/AddressBook.java
@@ -1,7 +1,27 @@
+/*
+ *@author: Shubham Pattar
+ *main code for address book management
+ *this file is the gateway for all functions
+ */
 package com.bridgelabz.fellowship.main;
-
+import com.bridgelabz.fellowship.utils.*;
 public class AddressBook {
     public static void main(String[] args) {
+        String fname,lname,address,city,state,zip,phone;
         System.out.println("Welcome To Address Book");
+        System.out.println("Enter First Name");
+        fname=InputUtil.getStringValue();
+        System.out.println("Enter Last Name");
+        lname=InputUtil.getStringValue();
+        System.out.println("Enter Address");
+        address=InputUtil.getStringValue();
+        System.out.println("Enter City");
+        city=InputUtil.getStringValue();
+        System.out.println("Enter State");
+        state=InputUtil.getStringValue();
+        System.out.println("Enter Zip");
+        zip=InputUtil.getStringValue();
+        System.out.println("Enter phone number");
+        phone=InputUtil.getStringValue();
     }
 }
