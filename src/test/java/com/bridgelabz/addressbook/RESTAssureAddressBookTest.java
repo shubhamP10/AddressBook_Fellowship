a4c94678b6148fe67034cea07dda6d371503c23b
“Shubham” <shubhampattar2@gmail.com>
2020-08-04 00:58:11 +0530

    ADDED Test Cases For CRUD Operations Using Rest Assure.

--- src/test/java/com/bridgelabz/addressbook/RESTAssureAddressBookTest.java
+++ src/test/java/com/bridgelabz/addressbook/RESTAssureAddressBookTest.java
@@ -1,11 +1,11 @@
 package com.bridgelabz.addressbook;
 
-import com.google.gson.Gson;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,14 +33,40 @@ public class RESTAssureAddressBookTest {
         Response response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .body("{\"Name\":\"nikhil\",\"Phone\":\"9898989898\",\"State\":\"TN\"}")
+                .body("{\"firstName\": \"Sunil\", \"lastName\": \"Kemp\", \"address\": \"Mudhol\", " +
+                        "\"city\": \"GGG\", \"state\": \"KA\", \"zipCode\": \"457885\"," +
+                        "\"phone\": \"9999999999\"}")
                 .when()
                 .post("/AddressBook/create");
         String responseString = response.asString();
-        JSONObject jsonObject = new Gson().fromJson(responseString,JSONObject.class);
-        int id = (int) jsonObject.get("id");
         response.then().body("id",Matchers.any(Integer.class));
         System.out.println(responseString);
     }
+
+    @Test
+    public void givenAddressBook_OnUpdate_ShouldReturnUpdatedAddressBook() {
+        Response response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{\"firstName\": \"Aniket\", \"lastName\": \"Dakhare\", \"address\": \"Nagpur\", " +
+                        "\"city\": \"Gondia\", \"state\": \"Bihar\", \"zipCode\": \"457885\"," +
+                        "\"phone\": \"9999999999\"}")
+                .when()
+                .put("/AddressBook/update/3");
+        response.then().body("id", Matchers.any(Integer.class));
+        response.then().body("firstName", Matchers.is("Aniket"));
+    }
+
+    @Test
+    public void givenAddressBookId_OnDelete_ShouldReturnSuccessStatus() {
+        Response response = RestAssured.delete("/AddressBook/delete/6");
+        int statusCode = response.getStatusCode();
+        MatcherAssert.assertThat(statusCode, CoreMatchers.is(200));
+        response = getPersonList();
+        System.out.println("AT END: " + response.asString());
+        response.then().body("id", Matchers.not(Integer.class));
+    }
 }
+
+
 // json-server --port 4000 --routes routes.json --watch RESTAssureJSONFile.json
